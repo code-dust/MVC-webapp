c8cd7ddf916447a6d4a5257974d12bfe6e3a4a68
femiehlar <folarniyi@gmail.com>
2023-03-28 04:37:32 -0800

    added main folders

---
+++ public/index.php
@@ -0,0 +1,27 @@
+
+<?php
+
+//Front Controller
+//PHP version 5.4
+//echo "<p>hello world</p>";
+//echo 'Requested URL = "'.$_SERVER['QUERY_STRING'].'"';	
+
+
+//Routing
+require'../Core/Router.php';
+
+$router = new Router();
+//echo get_class($router);
+
+//add the routes
+$router->add('',['controller'=>'Home','action'=>'index']);
+$router->add('posts',['controller'=>'Posts','action'=>'index']);
+$router->add('posts/new',['controller'=>'Posts','action'=>'new']);
+
+//Display the routing table
+echo '<pre>';
+var_dump($router->getRoutes());
+echo '</pre>';
+
+
+?>	
